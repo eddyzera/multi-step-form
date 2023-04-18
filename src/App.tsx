4597bdf62541f456dcdf88383e0642a4ea701ca1
ednestig <edgar@nestig.com>
2023-04-17 23:01:26 -0300

    style: configuration and use sass module

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import style from './app.module.scss'
 
 export const App: React.FunctionComponent = () => {
-  return <h1>hello world</h1>
+  return <h1 className={style.text}>hello world</h1>
 }
---
+++ src/app.module.scss.d.ts
@@ -0,0 +1,4 @@
+declare const classNames: {
+  readonly text: "text";
+};
+export = classNames;
