2bf9684b670e475ab5be36957a480d4015d81321
ednestig <edgar@nestig.com>
2023-04-24 13:25:52 -0300

    refactor: show error message

--- src/components/FormPersonalInfo/FormPersonalInfo.module.scss.d.ts
+++ src/components/FormPersonalInfo/FormPersonalInfo.module.scss.d.ts
@@ -1,6 +1,8 @@
 declare const classNames: {
   readonly formPersonalInfoContainer: "formPersonalInfoContainer";
   readonly formPersonalInfoLabel: "formPersonalInfoLabel";
+  readonly errorContainer: "errorContainer";
   readonly submiteContainer: "submiteContainer";
+  readonly errorContent: "errorContent";
 };
 export = classNames;
--- src/components/FormPersonalInfo/FormPersonalInfo.tsx
+++ src/components/FormPersonalInfo/FormPersonalInfo.tsx
@@ -6,9 +6,9 @@ import styles from './FormPersonalInfo.module.scss'
 import { Button } from '../Button/Button'
 
 const createPersonalInfoSchema = z.object({
-  name: z.string().min(3).toLowerCase().nonempty(),
-  email: z.string().email('Formato de e-mail invalido').nonempty(),
-  phone: z.string().nonempty()
+  name: z.string().toLowerCase().nonempty('This field is required'),
+  email: z.string().email('invalid email').nonempty('This field is required'),
+  phone: z.string().nonempty('This field is required')
 })
 
 type CreatePersonalInformationFormData = z.infer<typeof createPersonalInfoSchema>
@@ -24,17 +24,20 @@ export const FormPersonalInfo: React.FunctionComponent = () => {
 
   return (
     <form className={styles.formPersonalInfoContainer} onSubmit={handleSubmit(handlePersonalInfo)} >
-      <div className={styles.formPersonalInfoLabel}>
+      <div className={`${styles.formPersonalInfoLabel} ${errors.name ? styles.errorContainer : ''}`}>
         <label htmlFor="">Name</label>
         <input type="text" placeholder='e.g. Stephen King' {...register('name')}/>
+        {errors.name && <span className={styles.errorContent}>{errors.name.message}</span>}
       </div>
-      <div className={styles.formPersonalInfoLabel}>
+      <div className={`${styles.formPersonalInfoLabel} ${errors.email ? styles.errorContainer : ''}`}>
         <label htmlFor="">Email Address</label>
-        <input type="text" placeholder='e.g. stephenking@lorem.com' {...register('email')} />
+        <input type="email" placeholder='e.g. stephenking@lorem.com' {...register('email')} />
+        {errors.email && <span>{errors.email.message}</span>}
       </div>
-      <div className={styles.formPersonalInfoLabel}>
+      <div className={`${styles.formPersonalInfoLabel} ${errors.phone ? styles.errorContainer : ''}`}>
         <label htmlFor="">Phone Number</label>
         <input type="text" placeholder='e.g. +1 234 567 890' {...register('phone')} />
+        {errors.phone && <span>{errors.phone.message}</span>}
       </div>
       <div className={styles.submiteContainer}>
         <Button label='Next Step' variant='blue' />
