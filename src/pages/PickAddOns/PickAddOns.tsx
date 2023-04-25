894bd0727781d29f2521cfc8f923e7ee0178fc07
ednestig <edgar@nestig.com>
2023-04-25 14:43:13 -0300

    feat: created PickAddOns page

---
+++ src/pages/PickAddOns/PickAddOns.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Header } from '../../components'
+
+export const PickAddOns: React.FunctionComponent = () => {
+  return (
+    <>
+      <Header title='Pick add-ons' subTitle='Add-ons help enhance your gaming experience.' />
+    </>
+  )
+}
