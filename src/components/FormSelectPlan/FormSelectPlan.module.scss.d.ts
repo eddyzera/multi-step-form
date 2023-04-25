7ed5f8aa6dd3ec5c5c523c1be14a6279a9e3799e
ednestig <edgar@nestig.com>
2023-04-25 09:51:39 -0300

    feature: created Radio Group and Switch input

---
+++ src/components/FormSelectPlan/FormSelectPlan.module.scss.d.ts
@@ -0,0 +1,13 @@
+declare const classNames: {
+  readonly radioGroupContainer: "radioGroupContainer";
+  readonly radioGroupItem: "radioGroupItem";
+  readonly iconGroup: "iconGroup";
+  readonly arcadeIcon: "arcadeIcon";
+  readonly advancedIcon: "advancedIcon";
+  readonly proIcon: "proIcon";
+  readonly containerFlex: "containerFlex";
+  readonly switchContainer: "switchContainer";
+  readonly switchRoot: "switchRoot";
+  readonly switchThumb: "switchThumb";
+};
+export = classNames;
--- src/pages/SelectYourPlan/SelectYourPlan.tsx
+++ src/pages/SelectYourPlan/SelectYourPlan.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from '../../components'
+import { FormSelectPlan } from '../../components/FormSelectPlan/FormSelectPlan'
 
 export const SelectYourPlan: React.FunctionComponent = () => {
   return (
     <>
       <Header title='Select your plan' subTitle='You have the option of monthly or yearly billing.' />
+      <FormSelectPlan />
     </>
   )
 }
